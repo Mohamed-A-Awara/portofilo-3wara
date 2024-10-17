8724dae88a01409714989d83d3892046c6ecf5bf
MohamedAwara <mohamedaymanawara@gmail.com>
2024-10-17 01:57:53 -0700

    Awara Version 2.1.0

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,6 +20,18 @@ function Home() {
     useEffect(() => {
         aos.init({ duration: 2000 })
     }, [])
+
+    let faceLink = 'https://www.facebook.com/profile.php?id=100011196822139&mibextid=ZbWKwL'
+    let githubLink = 'https://github.com/Mohamed-A-Awara'
+    let linkedLink = 'https://www.linkedin.com/in/mohamed-ayman-awara-20ba69219/'
+
+
+    const openCV = ()=>{
+        window.open('https://drive.google.com/file/d/1XKO88qEW-Aq7277wRWZvKfQtoXpLQpNt/view?usp=drive_link' , "_blank")
+    }
+    const openLink = (link)=>{
+        window.open(link , "_blank")
+    }
     return (
         <>
             {/* Start Home */}
@@ -34,12 +46,18 @@ function Home() {
                     </div>
 
                     <div className="social-media" style={{ display: "flex", padding: "0 3rem" }}>
-                        <SocialIcon ><FaFacebookF /></SocialIcon>
-                        <SocialIcon ><FaGithub /></SocialIcon>
-                        <SocialIcon ><FaLinkedinIn /></SocialIcon>
+                        <SocialIcon onClick={()=> openLink(faceLink)}><FaFacebookF /></SocialIcon>
+                        <SocialIcon onClick={()=> openLink(githubLink)} ><FaGithub /></SocialIcon>
+                        <SocialIcon onClick={()=> openLink(linkedLink)} ><FaLinkedinIn /></SocialIcon>
                     </div>
 
-                    <button className='btnCv' style={{ padding: "0 3rem", marginLeft: "3rem" }}> Download CV</button>
+                    <button
+                        className='btnCv'
+                        style={{ padding: "0 3rem", marginLeft: "3rem" }}
+                        onClick={openCV}
+                    >
+                        Download CV
+                    </button>
                 </section>
                 <div className="line" data-aos='fade-right'></div>
 
